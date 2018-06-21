e456ccb7d15833637d41006a3ddd65d1f14d9de5
Ron <youyihao@gmail.com>
2018-06-21 17:13:30 +0800

    Complete all basic functions

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # encoding=utf8
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 def open_chrome():
     option = webdriver.ChromeOptions()
@@ -14,29 +17,59 @@ def operation_auth(cd_driver):
     url = "https://cas.shmtu.edu.cn/cas/login?service=http%3A%2F%2Fjwxt.shmtu.edu.cn%2Fshmtu%2Fhome%21index.action"
     cd_driver.get(url)
     username = cd_driver.find_element_by_id("username")
-    username.send_keys("201610733026")
+    username.send_keys("")
     password = cd_driver.find_element_by_id("password")
-    password.send_keys("050915")
+    password.send_keys("")
     cd_driver.find_element_by_xpath("//*[@id=\"fm1\"]/section[3]/input[4]").click()
     cd_driver.find_element_by_xpath("//*[@id=\"MLeft\"]/div/ul/li[6]/a/div").click()
-    sleep(2)
+
+    WebDriverWait(cd_driver, 10).until(
+        expected_conditions.presence_of_element_located(
+            (By.XPATH, "//*[@id=\"MLeft\"]/div/table[2]/tbody/tr[1]/td[1]/div[2]/a")
+        )
+    )
+
     cd_driver.find_element_by_xpath("//*[@id=\"MLeft\"]/div/table[2]/tbody/tr[1]/td[1]/div[2]/a").click()
-    for i in range(10, 20):
-        sleep(2)
-        cd_driver.find_element_by_xpath("// *[ @ id = \"evaluateForm\"] / table[2] / tbody / tr[" + str(i) + "] / td[6] / a").click()
-        sleep(2)
+    for i in range(1, 20):
+
+        WebDriverWait(cd_driver, 10).until(
+            expected_conditions.presence_of_element_located(
+                (By.XPATH, "// *[ @ id = \"evaluateForm\"] / table[2] / tbody / tr[" + str(i) + "] / td[6] / a")
+            )
+        )
+
+        course = cd_driver.find_element_by_xpath(
+            "// *[ @ id = \"evaluateForm\"] / table[2] / tbody / tr[" + str(i) + "] / td[6] / a"
+        )
+        if course.text == '已评估':
+            continue
+        else:
+            course.click()
+
+        WebDriverWait(cd_driver, 10).until(
+            expected_conditions.presence_of_element_located(
+                (By.CSS_SELECTOR, "b")
+            )
+        )
+
         questions_odd = cd_driver.find_elements_by_css_selector(".griddata-odd")
         questions_even = cd_driver.find_elements_by_css_selector(".griddata-even")
+        flag = 1
         for question in questions_odd:
             options = question.find_elements_by_css_selector("input")
             value = []
             for option in options:
                 value.append(option.get_attribute("value"))
             ans = max(value)
+            if flag == 6:
+                max1 = max(value)
+                min1 = min(value)
+                ans = value[0]
             for option in options:
                 if option.get_attribute("value") == ans:
                     option.click()
                     break
+            flag += 1
         flag = 1
         for question in questions_even:
             options = question.find_elements_by_css_selector("input")
@@ -45,8 +78,8 @@ def operation_auth(cd_driver):
                 value.append(option.get_attribute("value"))
             ans = max(value)
             if flag == 6:
-                value.remove(max(value))
-                value.remove(min(value))
+                max2 = max(value)
+                min2 = min(value)
                 ans = value[0]
             for option in options:
                 if option.get_attribute("value") == ans:
@@ -56,8 +89,68 @@ def operation_auth(cd_driver):
         cd_driver.find_element_by_xpath("//*[@id=\"content_1\"]").send_keys("都很有帮助。")
         cd_driver.find_element_by_xpath("//*[@id=\"content_2\"]").send_keys("已经很棒了。")
         cd_driver.find_element_by_xpath("//*[@id=\"btnSave\"]").click()
-        cd_driver.switch_to_alert().accept()
-        sleep(2)
+        msgbox = cd_driver.switch_to_alert()
+        if msgbox.text == "确认提交?":
+            cd_driver.switch_to_alert().accept()
+        else:
+            cd_driver.switch_to_alert().accept()
+            options1 = cd_driver.find_element_by_xpath("//*[@id=\"evaluateTB\"]/tr[12]/td[2]").\
+                find_elements_by_css_selector("input")
+            options2 = cd_driver.find_element_by_xpath("//*[@id=\"evaluateTB\"]/tr[11]/td[3]").\
+                find_elements_by_css_selector("input")
+            ans1 = max1
+            ans2 = min2
+            for option in options1:
+                if option.get_attribute("value") == ans1:
+                    option.click()
+                    break
+            for option in options2:
+                if option.get_attribute("value") == ans2:
+                    option.click()
+                    break
+            cd_driver.find_element_by_xpath("//*[@id=\"btnSave\"]").click()
+            msgbox = cd_driver.switch_to_alert()
+            if msgbox.text == "确认提交?":
+                msgbox.accept()
+            else:
+                cd_driver.switch_to_alert().accept()
+                options1 = cd_driver.find_element_by_xpath("//*[@id=\"evaluateTB\"]/tr[12]/td[2]"). \
+                    find_elements_by_css_selector("input")
+                options2 = cd_driver.find_element_by_xpath("//*[@id=\"evaluateTB\"]/tr[11]/td[3]"). \
+                    find_elements_by_css_selector("input")
+                ans1 = min1
+                ans2 = max2
+                for option in options1:
+                    if option.get_attribute("value") == ans1:
+                        option.click()
+                        break
+                for option in options2:
+                    if option.get_attribute("value") == ans2:
+                        option.click()
+                        break
+                cd_driver.find_element_by_xpath("//*[@id=\"btnSave\"]").click()
+                msgbox = cd_driver.switch_to_alert()
+                if msgbox.text == "确认提交?":
+                    msgbox.accept()
+                else:
+                    cd_driver.switch_to_alert().accept()
+                    options1 = cd_driver.find_element_by_xpath("//*[@id=\"evaluateTB\"]/tr[12]/td[2]"). \
+                        find_elements_by_css_selector("input")
+                    options2 = cd_driver.find_element_by_xpath("//*[@id=\"evaluateTB\"]/tr[11]/td[3]"). \
+                        find_elements_by_css_selector("input")
+                    ans1 = max1
+                    ans2 = max2
+                    for option in options1:
+                        if option.get_attribute("value") == ans1:
+                            option.click()
+                            break
+                    for option in options2:
+                        if option.get_attribute("value") == ans2:
+                            option.click()
+                            break
+                    cd_driver.find_element_by_xpath("//*[@id=\"btnSave\"]").click()
+                    msgbox = cd_driver.switch_to_alert()
+                    msgbox.accept()
 
     print('Succeed.')
 
